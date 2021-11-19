6e11c00018b683b2d4f166f1e1e142e6e957f0d3
madhurya <madhuryar017@gmail.com>
2021-11-19 18:49:12 +0530

    [madhurya] Add first welcome message

---
+++ RemoteGitPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoteGitPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!---------------");
+        }
+    }
+}
