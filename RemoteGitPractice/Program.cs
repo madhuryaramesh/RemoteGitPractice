c8d95c29b5fad52fae3ccc858453eeed5ea24d0d
madhurya <madhuryar017@gmail.com>
2021-11-19 18:57:50 +0530

    [madhurya] Adding UC1

--- RemoteGitPractice/Program.cs
+++ RemoteGitPractice/Program.cs
@@ -4,9 +4,27 @@ namespace RemoteGitPractice
 {
     class Program
     {
+        public const int isFulltime = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!---------------");
+            Console.WriteLine("Welcome UC1");
+            //int empHrs = 0;
+            //int empWage = 0;
+            Console.WriteLine("UC1");
+            Console.WriteLine("-------------------------------");
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == isFulltime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+
+            Console.WriteLine("-------------------------------");
         }
     }
 }
